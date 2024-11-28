59723ea21a467f90f3ca1874dcfe6946c96366bb
I573040 <elias.rw2@gmail.com>
2024-11-28 16:37:50 +0100

    Elias war hier

--- Frontend/Athlead/CompetitionsOverviewView.swift
+++ Frontend/Athlead/CompetitionsOverviewView.swift
@@ -15,17 +15,61 @@ struct Competition: Identifiable {
     let location: String
 }
 
+struct Placing : Identifiable {
+    let id = UUID()
+    let name: String
+    let place : Int8
+}
+
+
 struct CompetitionsOverviewView: View {
+    enum CompType {
+        case LongJump
+        case HundredMeterRun
+        case HighJump
+    }
+    
     let competitions = [
         Competition(name: "Weitsprung", date: "20.11.2024", location: "Stadion A"),
         Competition(name: "100m Lauf", date: "21.11.2024", location: "Stadion B"),
         Competition(name: "Hochsprung", date: "22.11.2024", location: "Stadion C")
     ]
+    
+    let longJumpPlacings = [
+        Placing(name: "Elias jump", place: 1),
+        Placing(name: "Jan jump", place: 2)
+    ]
+    
+    let hundredMeterRunPlacings = [
+        Placing(name: "Elias Ran", place: 1),
+        Placing(name: "Jan RAn", place: 2)
+    ]
+    
+    let highJumpPlacings = [
+        Placing(name: "Elias high jump", place: 1),
+        Placing(name: "Jan high jump", place: 2)
+    ]
+    
+    func getPlacings(for comp: String) -> [Placing]
+    {
+        switch(comp)
+        {
+        case "Weitsprung":
+            return longJumpPlacings;
+        case "100m Lauf":
+            return hundredMeterRunPlacings;
+        case "Hochsprung":
+            return highJumpPlacings;
+        default:
+            return [];
+        }
+        
+    }
 
     var body: some View {
         NavigationView {
             List(competitions) { competition in
-                NavigationLink(destination: CompetitionDetailView(competition: competition)) {
+                NavigationLink(destination: CompetitionDetailView(competition: competition, placings: getPlacings(for: competition.name))) {
                     HStack {
                         VStack(alignment: .leading) {
                             Text(competition.name)
@@ -44,6 +88,7 @@ struct CompetitionsOverviewView: View {
 
 struct CompetitionDetailView: View {
     let competition: Competition
+    let placings: [Placing]
 
     var body: some View {
         VStack(spacing: 20) {
@@ -52,7 +97,11 @@ struct CompetitionDetailView: View {
                 .bold()
             Text("Datum: \(competition.date)")
             Text("Ort: \(competition.location)")
-            Spacer()
+            List(placings, id: \.name) {
+                placing in Text(placing.name + " " + placing.place.formatted())
+            }
+            .padding(.top, 5)
+            .foregroundColor(.primary)
         }
         .padding()
         .navigationTitle("Details")
