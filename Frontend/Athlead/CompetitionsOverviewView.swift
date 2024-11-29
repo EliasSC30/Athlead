23ae0faa1f60df0904c8d3ceead087e76cff3827
I573040 <elias.rw2@gmail.com>
2024-11-29 08:15:47 +0100

    Add placing nr

--- Frontend/Athlead/CompetitionsOverviewView.swift
+++ Frontend/Athlead/CompetitionsOverviewView.swift
@@ -110,14 +110,15 @@ struct CompetitionDetailView: View {
                 .bold()
             Text("Datum: \(competition.date)")
             Text("Ort: \(competition.location)")
-            List(rowContent, id: \.self) { content in
-                Text(content)
+            List(rowContent.indices, id: \.self) { index in
+                Text((index + 1).formatted() + "  " + rowContent[index])
             }
             .padding(.top, 5)
             .foregroundColor(.primary)
             .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+            .background(Color.white)
+            .scrollContentBackground(.hidden)
         }
-        .padding()
-        .navigationTitle("Details")
+        
     }
 }
