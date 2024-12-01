ca3d168a30d13acc3b9955e61965a2a2a6c764ee
I573801 <jan.wichmann@sap.com>
2024-12-01 17:19:19 +0100

    kommen

--- Frontend/Athlead/JudgeOverviewView.swift
+++ Frontend/Athlead/JudgeOverviewView.swift
@@ -7,33 +7,32 @@
 
 import SwiftUI
 
-
-struct JudgeOverviewView : View {
+struct JudgeOverviewView: View {
     let competitions = ["100m Lauf", "Weitsprung", "Hochsprung"]
-    
+
     var body: some View {
-        print("REcompute!!!")
-        return
-        HStack {
-            NavigationView {
-                ScrollView {
-                        VStack(spacing: 12) {
-                            Text("Wettkämpfe").bold().font(.title)
-                            ForEach(competitions.indices, id: \.self) { index in
-                                NavigationLink("\(competitions[index])",
-                                               destination: JudgeContestView(COMPETITION: competitions[index])
-                                    ) // NavigationLink
-                                    .bold()
-                                    .background(Color.white)
-                                    .foregroundColor(Color.black)
-                                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
-                                    .padding(.vertical, 4)
-                                    .frame(maxWidth: .infinity).onAppear(){print("Appearing")}
-                            } // ForEach
-                            .padding(.top, 10)
-                        } // VStack
-                } // ScrollView
-            } // NavigationView
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 12) {
+                    Text("Wettkämpfe")
+                        .bold()
+                        .font(.title)
+                    ForEach(competitions.indices, id: \.self) { index in
+                        NavigationLink(destination: JudgeContestView(COMPETITION: competitions[index])) {
+                            Text(competitions[index])
+                                .bold()
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.white)
+                                .foregroundColor(Color.black)
+                                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+                        }
+                        .padding(.vertical, 4)
+                    }
+                }
+                .padding(.top, 10)
+            }
+            .background(Color(UIColor.systemGroupedBackground))
         }
     }
 }
