cacaef5241ff9521d9f358cce2399b4a23c40d57
I573040 <elias.rw2@gmail.com>
2024-12-04 20:08:33 +0100

    Yo

--- Backend/src/api/service.rs
+++ Backend/src/api/service.rs
@@ -17,49 +17,49 @@ pub fn config(conf: &mut web::ServiceConfig) {
 
         // Sportfest
         .service(sportfests_list_handler) // for now simply queries db content of sf table
-        .service(sportfests_create_handler) // works
-        .service(sportfests_get_masterview_handler) // works
+        .service(sportfests_create_handler)
+        .service(sportfests_get_masterview_handler)
         .service(sportfests_update_handler) // wip, takes id's not attributes
-        .service(create_contest_for_sf_handler) // works
+        .service(create_contest_for_sf_handler)
 
         // Details
-        .service(details_get_all_handler) // works
-        .service(details_get_by_id_handler) // works
+        .service(details_get_all_handler)
+        .service(details_get_by_id_handler)
         .service(details_create_handler) // works but needs location and contact-person id
         .service(details_update_handler) // wip, probably works but takes ids
 
         // Location
-        .service(locations_get_all_handler) // works
-        .service(locations_get_by_id_handler) // works
-        .service(locations_create_handler) // works
-        .service(locations_update_handler) // works
+        .service(locations_get_all_handler)
+        .service(locations_get_by_id_handler)
+        .service(locations_create_handler)
+        .service(locations_update_handler)
 
         // ContactInfos
-        .service(contactinfos_get_all_handler) // works
-        .service(contactinfos_create_handler) // works
-        .service(contactinfos_get_by_id_handler) // works
+        .service(contactinfos_get_all_handler)
+        .service(contactinfos_create_handler)
+        .service(contactinfos_get_by_id_handler)
         .service(contactinfos_update_handler) // works but two fields are missing
 
         // Persons
         .service(persons_get_all_handler) // wip, works but returns contactinfo id
-        .service(persons_get_by_id_handler) // works
+        .service(persons_get_by_id_handler)
         .service(persons_update_handler)// wip, works but only role and contactinfo_id can be updated
-        .service(persons_create_handler) // works
+        .service(persons_create_handler)
 
         // C_Templates
-        .service(ctemplates_get_all_handler) // works
-        .service(ctemplates_get_by_id_handler) // works
-        .service(ctemplate_create_handler) // works
+        .service(ctemplates_get_all_handler)
+        .service(ctemplates_get_by_id_handler)
+        .service(ctemplate_create_handler)
 
 
         // Contests
         // No getters since all fields are ids, only complex getters are useful
-        .service(contest_get_results_by_id_handler) // works
-        .service(contests_create_results) // works
-        .service(contests_create_handler) // works
-        .service(contests_get_master_view_handler) // works
+        .service(contest_get_results_by_id_handler)
+        .service(contests_create_results)
+        .service(contests_create_handler)
+        .service(contests_get_master_view_handler)
 
         // ContestResults
-        .service(contestresult_create_handler); // works
+        .service(contestresult_create_handler);
     conf.service(scope);
 }
